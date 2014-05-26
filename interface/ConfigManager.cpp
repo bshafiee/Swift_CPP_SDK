81392dccdbe7909d6ccc6898d13931ba4bca7605
Behrooz <bshafiee@uwatelroo.ca>
2014-05-26 17:36:04 -0400

    added config manager

---
+++ interface/ConfigManager.cpp
@@ -0,0 +1,36 @@
+/*
+ * ConfigManager.cpp
+ *
+ *  Created on: 2014-05-26
+ *      Author: Behrooz Shafiee Sarjaz
+ */
+
+#include "ConfigManager.h"
+
+namespace Swift {
+
+static ConfigManager::ConfigMap config;
+
+const string* ConfigManager::getProperty(ConfigKey key) {
+  ConfigManager::ConfigMap::Iterator it = config.find(key);
+  if(it == config.end())
+    return nullptr;
+  else
+    return &it->second;
+}
+
+int ConfigManager::putProperty(ConfigKey key, const string &value) {
+  config.insert(ConfigMap::ValueType(key, value));
+  return SWIFT_OK;
+}
+
+int ConfigManager::removeProperty(ConfigKey key) {
+  config.erase(key);
+  return SWIFT_OK;
+}
+
+ConfigManager::ConfigMap::Iterator beginIterator() {
+  return config.begin();
+}
+
+} /* namespace Swift */
---
+++ interface/ErrorNo.h
@@ -0,0 +1,13 @@
+/*
+ * ErrorNo.h
+ *
+ *  Created on: 2014-05-26
+ *      Author: Behrooz Shafiee Sarjaz
+ */
+
+#ifndef ERRORNO_H_
+#define ERRORNO_H_
+
+#define SWIFT_OK 0; //Successful
+
+#endif /* ERRORNO_H_ */
