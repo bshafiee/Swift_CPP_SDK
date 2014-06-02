41fb8a4d64d327df5a39ce83cb307df663609f71
Behrooz <bshafiee@uwatelroo.ca>
2014-06-02 17:56:19 -0400

    added interface files

---
+++ interface/SwiftAPI.cpp
@@ -0,0 +1,21 @@
+/*
+ * SwiftAPI.cpp
+ *
+ *  Created on: 2014-06-02
+ *      Author: Behrooz Shafiee Sarjaz
+ */
+
+#include "SwiftAPI.h"
+
+namespace Swift {
+
+SwiftAPI::SwiftAPI() {
+  // TODO Auto-generated constructor stub
+
+}
+
+SwiftAPI::~SwiftAPI() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace Swift */
---
+++ interface/SwiftAPI.h
@@ -0,0 +1,25 @@
+/*
+ * SwiftAPI.h
+ *
+ *  Created on: 2014-06-02
+ *      Author: Behrooz Shafiee Sarjaz
+ */
+
+#ifndef SWIFTAPI_H_
+#define SWIFTAPI_H_
+
+namespace Swift {
+
+class SwiftAPI {
+public:
+  SwiftAPI();
+  virtual ~SwiftAPI();
+
+  /** SWIFT API INTERFaces **/
+  //Accounts Operations
+  //Container Operations
+  //Object Operations
+};
+
+} /* namespace Swift */
+#endif /* SWIFTAPI_H_ */
